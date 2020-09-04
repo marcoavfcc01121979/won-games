3e773a8709d2b0c3c1191d4f861c19734d334380
marcoavfcc01121979 <marcoavfcc@gmail.com>
2020-09-04 09:08:30 -0300

    servicos

--- api/api/game/controllers/game.js
+++ api/api/game/controllers/game.js
@@ -5,4 +5,12 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  populate: (ctx) => {
+    console.log("Starting to populate....")
+
+    console.log(ctx.query)
+
+    ctx.send("FInished populating!")
+  }
+};
